8f8cda547eaa517bd3429d61402b91b3f43cf939
Aestdile <aestdile@gmail.com>
2024-10-22 19:52:03 +0500

    Add files via upload

---
+++ Module4Part1.cs
@@ -0,0 +1,76 @@
+﻿
+// Task-1
+
+/*
+
+int sum=0;
+
+for (int i=1; i<1000; i++)
+{
+    if (i%2!=0)
+    {
+        sum +=i;
+    }
+}
+Console.WriteLine($"Result: {sum}");
+
+
+
+// Task-2
+
+
+int[] arr={8, 2, 6, 12, 3, 9, 4, 5};
+int min=arr[0];
+int max=arr[0];
+
+for (int i=0; i<arr.Length; i++)
+{
+    if( arr[i]<min)
+    { 
+        min=arr[i];
+    }
+    if (arr[i]>max)
+    {
+        max=arr[i];
+    }
+}
+Console.WriteLine($"arr(max) * arr(min) = {max*min}");
+
+
+
+
+
+// Task-3
+
+
+Console.Write("x: ");
+int x=Convert.ToInt32(Console.ReadLine());
+int result=1;
+for (int i=1; i<=x; i++)
+{
+    result *=i;
+}
+Console.WriteLine($"result: {result}");
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
